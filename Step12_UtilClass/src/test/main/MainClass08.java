c46d4fffcd69544aba8ab82319a4bf5f65481ef5
leeseungwoo <oy10302@naver.com>
2023-11-06 17:45:29 +0900

    2023.11.06 Step12 내용추가

--- Step12_UtilClass/src/test/main/MainClass08.java
+++ Step12_UtilClass/src/test/main/MainClass08.java
@@ -12,8 +12,16 @@ public class MainClass08 {
 		 * value type 을 Object 로 지정하면 어떤 data type 이든지 다 담을 수 있다
 		 * 순서가 없는 데이터를 다룰 때 사용하면 됨
 		 * dto 클래스 대신에 사용하기도 하다.
+		 * 대신 꺼낼 때는 '캐스팅'으로 전환해서 사용해야한다.
 		 */
 		HashMap<String, Object> map = new HashMap<>();
-		map.get
+		map.put("num",1);
+		map.put("name","김구라");
+		map.put("isMan", true);
+		
+		// value 의 generic 클래스가 Object 이기 때문에 Object type 이 리턴된다.
+		int num=(int)map.get("num"); // 원래 type으로 casting 이 필요!
+		String name=(String)map.get("name");
+		boolean isMan=(boolean)map.get("isMan");
 	}
 }
